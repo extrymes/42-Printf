70214cc6c50f9bbb640df2768a380125c795ad7d
extrymes <simon.abras@gmail.com>
2024-05-17 21:56:08 +0200

    chore: update libft.h

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,12 +6,13 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/15 23:50:45 by sabras            #+#    #+#             */
-/*   Updated: 2024/05/16 20:12:06 by sabras           ###   ########.fr       */
+/*   Updated: 2024/05/17 21:54:53 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
+# define FORMATS "cspdiuxX%"
 # define BASE "0123456789abcdef"
 # define BASE_LEN 16
 # include <unistd.h>
@@ -20,12 +21,11 @@
 # include <stdarg.h>
 
 int		ft_printf(const char *str, ...);
-int		ft_print_data(va_list args, const char c, int *printed_chars);
-int		ft_putchar(int c, int *printed_chars);
-int		ft_putstr(char *str, int *printed_chars);
+void	ft_putchar(int c, int *printed_chars);
+void	ft_putstr(char *str, int *printed_chars);
 void	ft_puthexa(unsigned long value, int *printed_chars);
-int		ft_putptr(unsigned long int value, int *printed_chars);
-int		ft_putint(int nbr, int *printed_chars);
-int		ft_putuint(unsigned int nbr, int *printed_chars);
+void	ft_putptr(unsigned long value, int *printed_chars);
+void	ft_putint(int nbr, int *printed_chars);
+void	ft_putuint(unsigned int nbr, int *printed_chars);
 
 #endif
